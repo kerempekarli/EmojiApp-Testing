6900ad8c8aa9d18563f3a4a93e60cd7e9cdd8d34
kerempekarli <kerempekarli@gmail.com>
2022-01-30 19:49:21 +0300

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,39 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
+describe('all test', () => {
+  let emoji100, input;
+
+  beforeEach(() => {
+      render(<App />);
+      emoji100 = screen.getByText(/100/i)
+      input = screen.getByPlaceholderText(/search emoji/i)
+  })
+
+  it("should got header", () => {
+      const headerTitle = screen.getByText(/Emoji Search/i)
+      expect(headerTitle).toBeInTheDocument();
+  })
+
+  it("should render emoji list", () => {
+      expect(emoji100).toBeInTheDocument()
+  })
+
+  it("should filter", () => {
+      const emojiText = "grim"
+      userEvent.type(input, emojiText)
+      expect(emoji100).not.toBeInTheDocument()
+  })
+
+  it("should copy", () => {
+      //öncelikle execCommand'i mockluyoruz.
+      document.execCommand = jest.fn(); // execCommand detaylı bakmak için link: https://developer.mozilla.org/en-US/docs/Web/API/Document/execCommand
+      //uygulamayı ayağa kaldırıp console üzerinden document.execCommand("copy") yazdığınızda fareniz ile daha önce seçtiğiniz yeri kopyalar. execCommand'in bunun gibi fonksiyonları var.
+      userEvent.click(emoji100) //emoji100 üzerine tıklamayı sağlıyoruz
+
+      expect(document.execCommand).toBeCalledWith('copy') // burada execCommand'in 'copy' olarak çağırılıp çağırılmadığını soruyoruz
+      
+      const copyEmoji = window.ClipboardData; //kopyalanan datayı alıyoruz.
+      expect(copyEmoji).toEqual(emoji100.value)
+  })
 });
